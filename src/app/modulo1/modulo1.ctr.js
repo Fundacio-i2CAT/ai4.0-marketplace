6aced77c2c9289fd3bb427528884296b7bc0a55b
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-06-21 11:06:01 +0200

    fix

--- src/app/modulo1/modulo1.ctr.js
+++ src/app/modulo1/modulo1.ctr.js
@@ -1,14 +1,22 @@
 (function(){
-	'use strinct';
+	'use strict';
 	
 	angular
 		.module('marketplace')
 		.controller('Modulo1Controller', Modulo1Controller);
-	Modulo1Controller.$inject = [];
-	function Modulo1Controller (){
+
+	Modulo1Controller.$inject = ['toastr'];
+	function Modulo1Controller (toastr){
 		var vm = this;
 
 		vm.texto = "Index modulo1. Controller";
+	
+		function showToastr() {
+			toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+		}
+
+		vm.showToastr = showToastr;
+
 	}
 
 })(); 
