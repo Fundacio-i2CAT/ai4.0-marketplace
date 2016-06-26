3dc4403ffd034aeaa5cf8b1cf569205ae0ffbf71
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-06-26 20:29:09 +0200

    services mock data

--- src/app/services/ServiceController.ctr.js
+++ src/app/services/ServiceController.ctr.js
@@ -5,14 +5,39 @@
 		.module('marketplace')
 		.controller('ServiceController', ServiceController);
 
-		ServiceController.$inject = ['ServicesMock'];
-		function ServiceController (ServicesMock){
+		ServiceController.$inject = ['ServicesMock', '$timeout', 'ngProgressFactory', 'toastr', '$log'];
+		function ServiceController (ServicesMock, $timeout, ngProgressFactory, toastr, $log){
 			var vm = this;
 			vm.txt = 'Services Controller';
+			vm.services = ServicesMock;
+			vm.showTable = showTable;
+			vm.showToast = showToast;
 
-			vm.mockService = {};
-			vm.mockService.data = ServicesMock;
+			function progressBarConfigure (){
+				var progressbar = ngProgressFactory.createInstance();
+				progressbar.setHeight('4px');
+				progressbar.setColor('#00802b');
 
+				return progressbar;
+			}
+
+			function showTable(){
+				vm.progressbar = progressBarConfigure();
+				
+				vm.progressbar.start();
+				$timeout(function(){
+					vm.progressbar.complete();
+					vm.show = true;
+				}, 2000);
+				
+			}
+			vm.showTable();
+
+			function showToast(service) {
+				toastr.info('Confirmació enviada', service.name);
+				$log(service);
+				
+			}
 		
 		}
 
