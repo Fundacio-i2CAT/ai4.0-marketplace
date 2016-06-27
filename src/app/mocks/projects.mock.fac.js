57d8a4b60b34bb92fa1751aaf702543b4c008f61
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-06-27 16:14:01 +0200

    projects and services mocks

--- src/app/mocks/projects.mock.fac.js
+++ src/app/mocks/projects.mock.fac.js
@@ -10,11 +10,93 @@
 	function ProjecstMock(){
 		var projects = [
 			{
-
-			},
-			{
-
-			}
+	            "_id": "5770f22079374a13daa9838c",
+	            "status": 0, 
+	            "name": "project1", 
+	            "created_at": "2016-06-27 09:30:08.159000", 
+	            "updated_at": "2016-06-27 09:30:08.165000", 
+	            "summary": null, 
+	            "client": "5770f21e79374a13daa98388", 
+	            "services": [
+	                {
+	                    "context_type": "system",
+	                    "service_type": "system",  
+	                    "status": 0, 
+	                    "_id": "5770f22079374a13daa9838b", 
+	                    "name": "apache", 
+	                    "provider": {
+	                        "_id": "5770f21e79374a13daa98386", 
+	                        "name": "prov1"
+	                    }
+	                }
+	            ]
+	        },
+	        {
+	            "_id": "5770f22079374a13daa9838d",
+	            "status": 0, 
+	            "name": "project2", 
+	            "created_at": "2016-06-27 10:30:08.159000", 
+	            "updated_at": "2016-06-27 10:30:08.165000", 
+	            "summary": null, 
+	            "client": "5770f21e79374a13daa98388", 
+	            "services": [
+	                {
+	                    "context_type": "system",
+	                    "service_type": "system",  
+	                    "status": 0, 
+	                    "_id": "5770f22079374a13daa9838b", 
+	                    "name": "apache", 
+	                    "provider": {
+	                        "_id": "5770f21e79374a13daa98386", 
+	                        "name": "prov1"
+	                    }
+	                }
+	            ]
+	        },
+	        {
+	            "_id": "5770f22079374a13daa9838c",
+	            "status": 0, 
+	            "name": "project3", 
+	            "created_at": "2016-06-27 09:30:08.159000", 
+	            "updated_at": "2016-06-27 09:30:08.165000", 
+	            "summary": null, 
+	            "client": "5770f21e79374a13daa98388", 
+	            "services": [
+	                {
+	                    "context_type": "system",
+	                    "service_type": "system",  
+	                    "status": 0, 
+	                    "_id": "5770f22079374a13daa9838b", 
+	                    "name": "apache", 
+	                    "provider": {
+	                        "_id": "5770f21e79374a13daa98386", 
+	                        "name": "prov1"
+	                    }
+	                }
+	            ]
+	        },
+	        {
+	            "_id": "5770f22079374a13daa9838d",
+	            "status": 0, 
+	            "name": "project4", 
+	            "created_at": "2016-06-27 10:30:08.159000", 
+	            "updated_at": "2016-06-27 10:30:08.165000", 
+	            "summary": null, 
+	            "client": "5770f21e79374a13daa98388", 
+	            "services": [
+	                {
+	                    "context_type": "system",
+	                    "service_type": "system",  
+	                    "status": 0, 
+	                    "_id": "5770f22079374a13daa9838b", 
+	                    "name": "apache", 
+	                    "provider": {
+	                        "_id": "5770f21e79374a13daa98386", 
+	                        "name": "prov1"
+	                    }
+	                }
+	            ]
+	        }
 		];
 
 		return projects;
--- src/app/mocks/services.mock.fac.js
+++ src/app/mocks/services.mock.fac.js
@@ -32,10 +32,37 @@
 				"provider": "575aca9876e63a3a870ee462", 
 				"service_type": "cloud", 
 				"_id": "575aca9876e63a3a870ee466"
+			},
+			{
+				"name": "service3", 
+				"sectors": [
+					"industry",
+					"application",
+					"web technology"
+				], 
+				"created_at": "2016-06-10 14:11:36.379000", 
+				"updated_at": null, 
+				"summary": "summary 3", 
+				"provider": "575aca9876e63a3a870ee462", 
+				"service_type": "generic", 
+				"_id": "575aca9876e63a3a870ee465"
+			}, 
+			{
+				"name": "service4", 
+				"sectors": [
+					"automotion"
+				], 
+				"created_at": "2016-06-10 14:11:36.390000", 
+				"updated_at": null, 
+				"summary": "summary 4", 
+				"provider": "575aca9876e63a3a870ee462", 
+				"service_type": "cloud", 
+				"_id": "575aca9876e63a3a870ee466"
 			}
+
 		];
 
-		return services
+		return services;
 	}
 
 
