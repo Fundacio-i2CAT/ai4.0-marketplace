8f4680894085e7206760d40b5add07271b7ec468
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-06-28 15:27:38 +0200

    edited host

--- src/app/common/Connection.fac.js
+++ src/app/common/Connection.fac.js
@@ -3,40 +3,38 @@
 
 	angular
 		.module('marketplace')
-		.factory('ConnectionFactory', [function (){
+		.factory('ConnectionFactory', ConnectionFactory);
+	ConnectionFactory.$inject = [];
 
-			var host = "http://anella.i2cat.net:3000/";
+	function ConnectionFactory (){
 
-			/*
-			var headers = {
-				'Content-Type': 'application/json; charset=utf-8',
-				'X-Token': getToken()
-			}	
-		
-			function getToken() {
-				var security; //get data from cookie or localstorage
-				if (security) token = security.token;
-				return token;	
-			}
+		var host = "http://anella.i2cat.net:8010/";
 
-			function http(url, method, extraParams, data) {
-	            var headers = headersFn();
-	            return $http({
-	                method: method,
-	                url: validUrl(url, extraParams),
-	                data: data,
-	                config: {
-	                    cache: true
-	                },
-	                headers: headers
-	            });
-	        }*/
+		/*
+		var headers = {
+			'Content-Type': 'application/json; charset=utf-8',
+			'X-Token': getToken()
+		}	
+	
 
-			return {
-				host: host
-			};
+		function http(url, method, extraParams, data) {
+            var headers = headersFn();
+            return $http({
+                method: method,
+                url: validUrl(url, extraParams),
+                data: data,
+                config: {
+                    cache: true
+                },
+                headers: headers
+            });
+        }*/
+
+		return {
+			host: host
+		};
 
 
-		}]);
+	}
 
 })()
