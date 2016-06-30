228832706f50f461969387da420ce16ac857204c
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-06-30 15:35:51 +0200

    projects response.data.status instead of response.data.success

--- src/app/projects/ProjectController.ctr.js
+++ src/app/projects/ProjectController.ctr.js
@@ -5,15 +5,15 @@
 		.module('marketplace')
 		.controller('ProjectController', ProjectController);
 
-	ProjectController.$inject = ['toastr', 'ProjectFactory', '$log', 'UserFactory'];
-	function ProjectController (toastr, ProjectFactory, $log, UserFactory){
+	ProjectController.$inject = ['toastr', 'ProjectFactory', '$log', 'UserFactory', 'ProjectsMock', 'ngProgressFactory'];
+	function ProjectController (toastr, ProjectFactory, $log, UserFactory, ProjectsMock, ngProgressFactory){
 		var vm = this;
 		vm.model = {};
 
 		//getAll de projects (url falsa de users)
 		vm.getAll = function(){
 			ProjectFactory.getAll().then(function(response){
-					if (response.data.success === false) {
+					if (response.data.status === 'fail') {
 						// toastr.info('response.error');
 					} else {
 						vm.allProjects = response.data.result;
@@ -25,7 +25,7 @@
 			var user_name = vm.model.username;
 			$log.log(user_name);
 			UserFactory.getUserByName(user_name).then(function(response){
-				if (response.data.success === false) {
+				if (response.data.status === 'fail') {
 					//
 				} else {
 					vm.currentUser = response.data.result[0];
@@ -34,15 +34,38 @@
 		};
 
 		vm.getClientProjectsByPartnerId = function(partnerId) {
+			progressbar.start();
 			ProjectFactory.getClientProjectsByPartnerId(partnerId).then(function(response) {
-				if (response.data.success === false) {
-					//
+				if (response.data.status === 'fail') {
+					toastr.info('Hi ha hagut un errror al obtenir els projectes...', 'Nasti de plasti');
 				} else {
 					vm.allClientProjects = response.data.result;
-					$log.log(response.result);
+					toastr.info('Projectes relacionats amb el seu compte de client', 'Everything flows');
+				}
+			});
+			vm.progressbar.complete();
+		};
+
+		vm.getProviderProjectsByPartnerId = function (partnerId){
+			ProjectFactory.getProviderProjectsByPartnerId(partnerId).then(function(response) {
+				if (response.data.status === 'fail') {
+					toastr.info('Hi ha hagut un errror al obtenir els projectes...', 'Nasti de plasti');
+				} else {
+					toastr.info('Projectes relacionats amb el seu compte de proveïdor', 'Everything flows');
+					vm.allProviderProjects = ProjectsMock;
+					// vm.allProviderProjects = response.data.result;					
 				}
 			});
 		};
+
+		function progressBarConfigure (){
+			var progressbar = ngProgressFactory.createInstance();
+			progressbar.setHeight('4px');
+			progressbar.setColor('#00802b');
+
+			vm.progressbar = progressbar;
+			return progressbar;
+		}
 	}
 
 })();
