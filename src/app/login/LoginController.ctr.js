3a0bbab627b00a1619a6698b371cf2c626575d6e
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-06-30 11:39:26 +0200

    login and register view

--- src/app/login/LoginController.ctr.js
+++ src/app/login/LoginController.ctr.js
@@ -5,9 +5,9 @@
 		.module('marketplace')
 		.controller('LoginController', LoginController);
 
-		LoginController.$inject=['$location', 'toastr'];
+		LoginController.$inject=['$location', 'toastr', 'CurrentUserFactory'];
 
-		function LoginController ($location, toastr){
+		function LoginController ($location, toastr, CurrentUserFactory){
 			var vm = this;
 			vm.logModel = {};
 			vm.doLogin = doLogin;
@@ -15,16 +15,22 @@
 			function doLogin () {
 				var username = vm.logModel.username;
 				var pass = vm.logModel.password;
-
+				var user = {
+					username: username,
+					password: pass
+				}
 				if (username === 'admin' && pass === 'admin') {
-					// toastr.info('Login realitzat amb èxit', 'Benvingut');
+					CurrentUserFactory.setUser(user);
 					$location.path('services');
-		
 				} else {
 					toastr.info('El username o el password no coincideixen', 'Error de Login');
 					$location.path('login');
 					vm.logModel = {};
 				}
+			}
+
+			function doRegister () {
+				
 			}
 
 		}
