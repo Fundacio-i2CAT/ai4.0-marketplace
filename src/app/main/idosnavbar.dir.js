0db3331d1876221d8a2d7a47bc0ca91c5801e295
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-07-02 15:10:51 +0200

    main header menu

--- src/app/main/idosnavbar.dir.js
+++ src/app/main/idosnavbar.dir.js
@@ -4,18 +4,19 @@
 		.module('marketplace')
 		.directive('idosnavbar', idosnavbar);
 
-	idosnavbar.$inject = [];
+	idosnavbar.$inject = ['CurrentUserFactory'];
 	
 	/**
 	* Directiva que proporciona el template del header de la aplicacion
 	*/
-	function idosnavbar(){
+	function idosnavbar(CurrentUserFactory){
 		var directive = {
 			restrict: 'E',
-			templateUrl: 'app/main/navbar.tpl.html'/*,
+			templateUrl: 'app/main/navbar.tpl.html',
+			scope: true,
 			link: function (scope, element, attrs) {
-				
-			}*/
+
+			}
 		};
 		return directive;
 	}
