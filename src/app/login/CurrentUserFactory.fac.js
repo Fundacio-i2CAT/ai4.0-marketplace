dbda1f16b4f3f10d3d317bb4af4ac88e8f0df23a
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-07-02 15:10:22 +0200

    new getters and setters for user roles

--- src/app/login/CurrentUserFactory.fac.js
+++ src/app/login/CurrentUserFactory.fac.js
@@ -7,24 +7,42 @@
 
 	CurrentUserFactory.$inject = [];
 	function CurrentUserFactory() {
-		var currentUser = {};
+		var currentUser = {
+			user: {},
+			role: {}
+		};
 
 		function getUser() {
 			return currentUser;
 		}
 
 		function setUser(user) {
-			currentUser = user;
+			currentUser.user = user;
+			console.log(user);
 		}
 
 		function removeCurrentUser() {
-			currentUser = {};
+			currentUser.user = {};
+			currentUser.role = {};
 		}
 
+		function setProviderRole(){
+			currentUser.role = 'provider';
+		}
+
+		function setClientRole(){
+			currentUser.role = 'client';
+		}
+
+		function getRole() {
+			return (currentUser.role) ? currentUser.role : null;
+		}
 		return {
 			getUser: getUser,
 			setUser: setUser,
-			removeCurrentUser: removeCurrentUser
+			removeCurrentUser: removeCurrentUser,
+			setProviderRole: setProviderRole,
+			getRole: getRole
 		};
 
 	}
