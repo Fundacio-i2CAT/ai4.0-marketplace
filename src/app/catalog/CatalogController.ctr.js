1038fbdc0f3783b2470a8c721d677559cdfeabb1
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-07-02 15:07:56 +0200

    catalog module

---
+++ src/app/catalog/CatalogController.ctr.js
@@ -0,0 +1,12 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('marketplace')
+		.controller('CatalogController');
+	CatalogController.$inject = [];
+	function CatalogController() {
+		
+	}
+
+})();
