3d34e2d72226a95e15280158a19a0c878c5b4dc3
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-07-02 15:11:48 +0200

    tatami zone

---
+++ src/app/tatami/TatamiController.ctr.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('marketplace')
+		.controller('TatamiController', TatamiController);
+
+	TatamiController.$inject = [];
+	function TatamiController() {
+		
+	}
+
+})();
