725cd43c013bb2f2918e3fbe7f1a7ab1b75623f1
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-07-06 19:54:47 +0200

    back button service-edit view

--- src/app/catalog/catalog.js
+++ src/app/catalog/catalog.js
@@ -1,6 +1,6 @@
-(function(){
+/*(function(){
 	angular.element('li a').on('click', function(){
 		angular.element('li a').removeClass('active');
 		angular.element(this).addClass('active');
 	})
-})();
+})();*/
