6f5b5a8640389ded383de392a71c290bba0283a1
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-07-25 12:01:47 +0200

    removed unused line of code

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -10,9 +10,6 @@
 
     $log.debug('runBlock end');
 
-    // if (user) $http.defaults.headers.common.Authorization = 'anella=' + user.session_id;
-    // if (user) $http.defaults.headers.common['Set-Cookie'] += user.session_id;
-
   }
 
 })();
