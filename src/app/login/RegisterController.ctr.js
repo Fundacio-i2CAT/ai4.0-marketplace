64e3747f2793d7fcc7e749429c9f4226cb7b41b5
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-09-26 15:44:50 +0200

    register functionality in progress

--- src/app/login/RegisterController.ctr.js
+++ src/app/login/RegisterController.ctr.js
@@ -9,10 +9,28 @@
 
 	 	function RegisterController () {
 	 		var vm = this;
-	 		vm.focusPassword = false;
+	 		var passwordOk;
+	 		var showRegisterError = false;
+/*	 		vm.focusPassword = false;*/
+
+			function validPassword (pass, repeat_pass) {
+				return pass === repeat_pass;
+			}
 
 	 		vm.doRegister = function(credentials) {
-	 			console.log(credentials);
-	 		}
+	 			//if password and repeatpassword not equals an error must be shown
+	 			//and ask for repeat password again
+	 			passwordOk = validPassword(credentials.password, credentials.repeatpassword);
+	 			if (!passwordOk) {
+	 				// vm.credentials = credentials;
+	 				vm.showRegisterError = true;
+	 				vm.credentials.repeatpassword = '';
+	 				vm.credentials.password = '';
+	 				return;
+	 			} else {
+	 				
+	 			}
+
+	 		};
 	 	}
 })();
