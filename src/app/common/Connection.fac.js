80d6f5d91d2ca2b295a9f703a0dd7a924cae3abe
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-09-27 16:37:25 +0200

    changed url

--- src/app/common/Connection.fac.js
+++ src/app/common/Connection.fac.js
@@ -8,7 +8,8 @@
 
 	function ConnectionFactory (){
 
-		var host = "http://anella.i2cat.net:8010/";
+		// var host = "http://anella.i2cat.net:8010/";
+		var host = "http://anella.i2cat.net:9999/";
 		var localHostDavid = "http://192.168.10.70:8010/";
 
 		return {
