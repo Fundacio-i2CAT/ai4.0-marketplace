25a9d786d1dac661ba695014d9176f855295b257
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-10-03 09:46:36 +0200

    added new backend urls

--- src/app/common/Connection.fac.js
+++ src/app/common/Connection.fac.js
@@ -8,9 +8,10 @@
 
 	function ConnectionFactory (){
 
-		var hostProd = "http://anella.i2cat.net:8010/";
-		var hostDev = "http://anella.i2cat.net:9999/";
+		var production = "http://anella.i2cat.net:8010/";
+		var development = "http://anella.i2cat.net:9999/";
 		var localHostDavid = "http://192.168.10.70:8010/";
+		var localHostDavid2 = "http://192.168.122.1:8010/";
 
 		return {
 			host: localHostDavid
