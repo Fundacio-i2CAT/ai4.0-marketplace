b4a8028aae06a4ebb7e25d37a9b98928186385b4
Almiñana, Enric <enric.alminyana@i2cat.net>
2016-10-04 12:42:40 +0200

    new view and controller for forgotten password

---
+++ src/app/login/ForgotPassController.ctr.js
@@ -0,0 +1,24 @@
+(function () {
+	'use strict';
+
+	angular
+		.module('marketplace')
+		.controller('ForgotPassController', ForgotPassController);
+
+	ForgotPassController.$inject = [];
+	
+	function ForgotPassController() {
+		var vm = this;
+
+		vm.sendMail = function(credentials) {
+			console.log(credentials);
+			var destiny = credentials.mail;
+			var texte = "Texto de prueba";
+			/*var link = "mailto:" + destiny
+			             + "?cc=myCCaddress@example.com"
+			             + "&subject=" + encodeURI("This is my subject")
+			             + "&body=" + texte;
+			window.location.href = link;*/
+		};
+	}
+})()
