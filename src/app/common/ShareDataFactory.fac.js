2106f980b8de0335e817f8d3c33750e1989113ea
enric_alminana <enric.alminyana@i2cat.net>
2016-11-30 17:44:09 +0100

    new Factory for sharing data between controllers

---
+++ src/app/common/ShareDataFactory.fac.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('marketplace')
+		.factory('ShareDataFactory', ShareDataFactory);
+
+	ShareDataFactory.$inject = [];
+	function ShareDataFactory() {
+		var data;
+
+		
+	}
+})();
