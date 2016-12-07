fa59fc6bc3b34fe61dd4ae37a3f9ab4333cedf27
enric_alminana <enric.alminyana@i2cat.net>
2016-12-07 14:38:38 +0100

    on submit, building json object funcitonality moved to a function in order to separate differents things and edited this json object

--- src/app/projects/publish_service/PublishSrvController.ctr.js
+++ src/app/projects/publish_service/PublishSrvController.ctr.js
@@ -16,7 +16,7 @@
 		vm.showbar = false;
 
 		vm.types = [{name: 'Number'}, {name: 'String'}, {name: 'Url'}, {name: 'Vdi'}];
-		vm.discImageFormat = [{name: 'QCOW'}, {name: 'VDI'}];
+		vm.discImageFormat = [{name: 'QCOW2'}, {name: 'VDI'}];
 		vm.flavors = [
 			{
 				id: 1,
@@ -60,6 +60,15 @@
 
 		/* codigo para formulario estatico*/
 		vm.launchTemplate = function(srv) {
+			var model = buildPublishServiceJSON(srv);
+			ServiceFactory.createService(model).then(function (response){
+				console.log(response);
+			}, function (error) {
+				console.log(error);
+			});
+		};
+
+		function buildPublishServiceJSON (srv) {
 			var provider = getCurrentProvider('user');
 			var imageData = SaveImageDataService.getImageData();
 			
@@ -72,45 +81,36 @@
 				consumer_params: [
 					{
 						path: srv.path,
-						fields: [
-							{
-								name: srv.field1.name,
-								required: srv.field1.required,
-								desc: srv.field1.desc
-							},
-							{
-								name: srv.field2.name,
-								required: srv.field2.required,
-								desc: srv.field2.desc
-							},
-							{
-								name: (srv.field3.name) ? srv.field3.name : '',
-								required: (srv.field3.required) ? srv.field3.required : '',
-								desc: (srv.field3.desc) ? srv.field3.desc : '' 
-							}
-						]
+						fields: []
 					}
 				],
 				flavor: srv.flavor.name,
 				name_image: imageData.name_image,
 				vm_image: imageData.vm_image,
-				vm_image_format: imageData.vm_image_format,
+				vm_image_format: (srv.diskImageType == undefined || srv.diskImageType == null) ? 'qcow2' : srv.diskImageType,
 				runtime_params: [
 					{
-						name: srv.field1.name,
-						required: srv.field1.required,
-						desc: srv.field1.desc
+						name: srv.fields.field1.name,
+						required: srv.fields.field1.required,
+						desc: srv.fields.field1.desc
 					}
 				]
 			};
-			ServiceFactory.createService(srvToSave).then(function (response){
-				console.log(response);
-			}, function (error) {
-				console.log(error);
-			});
-		};
 
-		function buildJson() {}
+			//obtenir els templates dinamicament
+			if (srv.fields) {
+				angular.forEach(srv.fields, function (field, index){
+					console.log(field);
+					var field = {
+						name: field.name,
+						required: field.required,
+						desc: field.type.name
+					};
+					srvToSave.consumer_params.fields.push(field);
+				});
+			}
+			return srvToSave;
+		}
 
 		function getCurrentProvider(key) {
 			return LocalStorageFactory.getValue(key);
