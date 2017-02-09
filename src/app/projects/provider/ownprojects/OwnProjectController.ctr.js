06f6935908dd0e6b644a448a8205c98af81d78ec
enric <enric.alminyana@i2cat.net>
2017-02-09 18:01:48 +0100

    get all OwnProjects

--- src/app/projects/provider/ownprojects/OwnProjectController.ctr.js
+++ src/app/projects/provider/ownprojects/OwnProjectController.ctr.js
@@ -5,12 +5,21 @@
     .module('marketplace')
     .controller('OwnProjectController', OwnProjectController);
 
-    OwnProjectController.$inject = [];
+    OwnProjectController.$inject = ['OwnProjectFactory', 'toastr'];
 
-    function OwnProjectController() {
+    function OwnProjectController(OwnProjectFactory, toastr) {
       var vm = this;
       vm.text = 'OwnProjectController';
 
+      vm.getAllOwnProjects = function() {
+        OwnProjectFactory.getAllOwnProjects().then(function(response){
+          vm.allMyProjects = response;
+        }, function (error){
+          console.log(error);
+        })
+      };
+
+      vm.getAllOwnProjects();
 
 
     }
