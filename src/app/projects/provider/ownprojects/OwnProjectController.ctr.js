b282177f2a8d99783fc9dde63992cad0c764c623
enric <enric.alminyana@i2cat.net>
2017-02-10 11:33:26 +0100

    split all projects by activated property true or false

--- src/app/projects/provider/ownprojects/OwnProjectController.ctr.js
+++ src/app/projects/provider/ownprojects/OwnProjectController.ctr.js
@@ -10,17 +10,36 @@
     function OwnProjectController(OwnProjectFactory, toastr) {
       var vm = this;
       vm.text = 'OwnProjectController';
+      vm.allMyActiveProjects = [];
+      vm.allMyInactiveProjects = [];
+      //sortin table
+      vm.sortType = 'created_at';
+  		vm.sortReverse = true;
+
+
 
       vm.getAllOwnProjects = function() {
         OwnProjectFactory.getAllOwnProjects().then(function(response){
-          vm.allMyProjects = response;
+          activeProjects(response.result);
         }, function (error){
           console.log(error);
         })
       };
 
       vm.getAllOwnProjects();
-
+      /**
+      * split all projects in active/unactive project lists scope variables
+      * @param {objectArray} list all myProjects
+      */
+      function activeProjects(list) {
+        angular.forEach(list, function (each){
+          if (each.activated === true) {
+            vm.allMyActiveProjects.push(each);
+          } else if (each.activated === false) {
+            vm.allMyInactiveProjects.push(each);
+          }
+        });
+      }
 
     }
 })();
