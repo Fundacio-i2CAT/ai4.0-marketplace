ef2f0f845d97417b821bcb94550c55b3d4d6177f
enric <enric.alminyana@i2cat.net>
2017-03-23 09:41:22 +0100

    linter

--- src/app/uploadfile/fileUploadService.srv.js
+++ src/app/uploadfile/fileUploadService.srv.js
@@ -4,8 +4,8 @@
 	angular
 		.module('marketplace')
 		.service('fileUpload', fileUpload);
-		fileUpload.$inject = ['$http', 'SaveImageDataService', 'toastr', '$rootScope'];
-		function fileUpload($http, SaveImageDataService, toastr, $rootScope) {
+		fileUpload.$inject = ['$http', 'SaveImageDataService', 'toastr', '$rootScope', '$log'];
+		function fileUpload($http, SaveImageDataService, toastr, $rootScope, $log) {
 			this.uploadFileToUrl = function(file, uploadUrl){
 				var fd = new FormData();
 				fd.append('file', file);
@@ -19,6 +19,7 @@
 					toastr.success('Imatge de disc carregada amb èxit','Càrrega Imatge Disc');
 				})
 				.error(function(error){
+					$log.log(error);
 					toastr.error('Hi ha hagut un error i no s\'ha pogut pujar la Imatge','Càrrega Imatge Disc');
 				});
 			}
