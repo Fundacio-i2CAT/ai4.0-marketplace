c9943f0afd43a90f8cbb8f682b3abef287fdec4a
enric <enric.alminyana@i2cat.net>
2017-03-23 08:51:58 +0100

    solved linter console errors

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -5,7 +5,6 @@
     .module('marketplace')
     .config(config)
     .config(addInterceptor);
-    // .config(securithation);
 
   /** @ngInject */
   function config($logProvider, toastrConfig, localStorageServiceProvider) {
@@ -18,10 +17,6 @@
         .setStorageType('localStorage') //available: sessionStorage and localStorage
         .setNotify(true, true);
 
-    //$http, $httpProvider
-    /*$httpProvider.defaults.headers.common['Set-Cookie'] = 'anella=';
-    console.log($httpProvider);*/
-
     // toastr / Set options third-party lib
     toastrConfig.allowHtml = true;
     toastrConfig.timeOut = 5000;
@@ -32,20 +27,18 @@
     toastrConfig.progressBar = false;
     toastrConfig.closeButton = true;
 
-    //spinner
-    // usSpinnerConfigProvider.setDefaults({color: 'orange'});
   }
 
-  function securithation($httpProvider) {
-    var user, token;
-    if (window.localStorage['mkp.user']) {
-      console.log(angular.fromJson(window.localStorage['mkp.user']));
-      user = angular.fromJson(window.localStorage['mkp.user']);
-      token = user.user.token;
-      // userb = window.localStorage.user.token;
-    }
-    $httpProvider.defaults.headers.common['Authorization'] = token;
-  }
+  // function securithation($httpProvider) {
+  //   var user, token;
+  //   if (window.localStorage['mkp.user']) {
+  //     console.log(angular.fromJson(window.localStorage['mkp.user']));
+  //     user = angular.fromJson(window.localStorage['mkp.user']);
+  //     token = user.user.token;
+  //     // userb = window.localStorage.user.token;
+  //   }
+  //   $httpProvider.defaults.headers.common['Authorization'] = token;
+  // }
 
   function addInterceptor($httpProvider) {
     $httpProvider.interceptors.push('InterceptorService');
